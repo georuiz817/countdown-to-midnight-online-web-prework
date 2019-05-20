619983ee1b88b6edf5bbc53a2b8d571c272949bb
George Ruiz <georuiz817@gmail.com>
2019-05-20 15:19:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,14 @@ def countdown(number)
 end
 
 countdown(10)
+
+def countdown_with_sleep(number)
+  while number > 0
+   sleep 1
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+countdown(10)
